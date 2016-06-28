edf0348d66c42b71f1a00ee6d9b28af3547a86a5
Florian Büther <buether@itm.uni-luebeck.de>
2016-06-28 10:55:02 +0200

    Generated Schablone.

---
+++ src/main/java/eu/organicity/accounts/permissions/App.java
@@ -0,0 +1,13 @@
+package eu.organicity.accounts.permissions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "Hello World!" );
+  }
+}
